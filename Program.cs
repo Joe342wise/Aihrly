e811b3a03c33f33ce1d3199eb598340de04bf1fc
Joseph Osei Yaw Nyarko <jnyarko342@gmail.com>
2026-04-30 01:31:09 +0000

    feat: add DotNetEnv package and configure PostgreSQL connection in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
+using Aihrly.Data;
+using Microsoft.EntityFrameworkCore;
+
+DotNetEnv.Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = $"Host={Environment.GetEnvironmentVariable("POSTGRES_HOST")};" +
+                       $"Port={Environment.GetEnvironmentVariable("POSTGRES_PORT")};" +
+                       $"Database={Environment.GetEnvironmentVariable("POSTGRES_DB")};" +
+                       $"Username={Environment.GetEnvironmentVariable("POSTGRES_USER")}";
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddDbContext<AihrlyDbContext>(options => options.UseNpgsql(connectionString));
+
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -12,28 +25,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- obj/Debug/net10.0/Aihrly.AssemblyInfo.cs
+++ obj/Debug/net10.0/Aihrly.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aihrly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3709ca6488f83ebf1c38c8f7a24074ff44e38af6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34b3dca1edb62b3a4148ffd3ec0f88445056a396")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aihrly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aihrly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
